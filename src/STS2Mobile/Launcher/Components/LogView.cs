64841842e65df3f18d4ae5e2fdec6325ad66eebf
dreamyloong2005 <siyuantao.wm.chn@gmail.com>
2026-04-07 00:23:39 +0800

    Create LogView.cs

---
+++ src/STS2Mobile/Launcher/Components/LogView.cs
@@ -0,0 +1,35 @@
+using Godot;
+
+namespace STS2Mobile.Launcher.Components;
+
+public class LogView : RichTextLabel
+{
+    public LogView(float scale)
+    {
+        CustomMinimumSize = new Vector2(0, (int)(120 * scale));
+        ScrollFollowing = true;
+        BbcodeEnabled = true;
+        AddThemeFontSizeOverride("normal_font_size", (int)(11 * scale));
+        AddThemeColorOverride("default_color", new Color(0.6f, 0.6f, 0.65f));
+
+        var bg = new StyleBoxFlat
+        {
+            BgColor = new Color(0.05f, 0.05f, 0.07f)
+        };
+        bg.SetCornerRadiusAll((int)(4 * scale));
+        bg.ContentMarginLeft = (int)(8 * scale);
+        bg.ContentMarginRight = (int)(8 * scale);
+        bg.ContentMarginTop = (int)(4 * scale);
+        bg.ContentMarginBottom = (int)(4 * scale);
+        AddThemeStyleboxOverride("normal", bg);
+    }
+
+    public void AppendLog(string msg) => AddText(msg + "\n");
+
+    public void AppendColoredLog(string msg, Color color)
+    {
+        PushColor(color);
+        AddText(msg + "\n");
+        Pop();
+    }
+}
